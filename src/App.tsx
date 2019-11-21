dc825a568fd65518fb638939a623e5c62f0f20a7
Ilya Zaitsev <godested@gmail.com>
2019-11-21 20:58:28 +0200

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { HelmetProvider, Helmet } from 'react-helmet-async';
 import { Layout } from 'core/components/Layout';
 import { ResumeProvider } from 'core/providers/Resume';
 import { Router } from 'core/routes';
+import { BASE_URL } from 'core/utils/vars';
 import 'normalize.css';
 import 'core/styles/global.scss';
 
@@ -11,7 +12,7 @@ export function App() {
   return (
     <HelmetProvider>
       <Helmet defaultTitle="Zaitsev Ilya" titleTemplate="%s | Zaitsev" />
-      <BrowserRouter>
+      <BrowserRouter basename={BASE_URL}>
         <ResumeProvider>
           <Layout>
             <Router />
--- src/core/utils/vars.ts
+++ src/core/utils/vars.ts
@@ -1,1 +1,2 @@
 export const IS_DEV = process.env.REACT_APP_NODE_ENV !== 'production';
+export const BASE_URL = process.env.PUBLIC_URL;
