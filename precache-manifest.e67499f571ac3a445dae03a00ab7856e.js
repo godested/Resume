d45be622cc943536d216429ed992adc1043b3838
Ilya Zaitsev <godested@gmail.com>
2019-11-21 20:43:24 +0200

    Initial build subtree commit

---
+++ precache-manifest.e67499f571ac3a445dae03a00ab7856e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b520a4ce40fa767ef4987d9ce49c63",
+    "url": "/godested/resume/index.html"
+  },
+  {
+    "revision": "1457d162e255bea4e68a",
+    "url": "/godested/resume/static/css/3.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b2e606b1188fd50e68bb",
+    "url": "/godested/resume/static/css/completion.d94c2d7c.chunk.css"
+  },
+  {
+    "revision": "2a6984dbe8f3d1eb81bd",
+    "url": "/godested/resume/static/css/main.b3918ccc.chunk.css"
+  },
+  {
+    "revision": "1457d162e255bea4e68a",
+    "url": "/godested/resume/static/js/3.ab14c5f7.chunk.js"
+  },
+  {
+    "revision": "bc3785bcfe3701ca6e27",
+    "url": "/godested/resume/static/js/4.332088ad.chunk.js"
+  },
+  {
+    "revision": "b2e606b1188fd50e68bb",
+    "url": "/godested/resume/static/js/completion.09cf6f7f.chunk.js"
+  },
+  {
+    "revision": "2a6984dbe8f3d1eb81bd",
+    "url": "/godested/resume/static/js/main.69b825d4.chunk.js"
+  },
+  {
+    "revision": "c76fa294a00cc37a1307",
+    "url": "/godested/resume/static/js/runtime-main.f74565d3.js"
+  }
+]);
