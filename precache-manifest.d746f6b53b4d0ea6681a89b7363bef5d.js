c470b85cef942f198bd7621a50d89c498a046788
Ilya Zaitsev <godested@gmail.com>
2019-12-09 16:33:03 +0200

    Updates

---
+++ precache-manifest.d746f6b53b4d0ea6681a89b7363bef5d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ceb650b7c2364f81677acc319d1d6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fafc8d6f79b995d416e",
+    "url": "/static/css/3.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d904223eba18a4f2089b",
+    "url": "/static/css/home.e24ae894.chunk.css"
+  },
+  {
+    "revision": "5fcf0374087f28041612",
+    "url": "/static/css/main.39026c23.chunk.css"
+  },
+  {
+    "revision": "4fafc8d6f79b995d416e",
+    "url": "/static/js/3.92e33608.chunk.js"
+  },
+  {
+    "revision": "d904223eba18a4f2089b",
+    "url": "/static/js/home.e501b2fc.chunk.js"
+  },
+  {
+    "revision": "5fcf0374087f28041612",
+    "url": "/static/js/main.e6ab8b18.chunk.js"
+  },
+  {
+    "revision": "adfabc6669a1ca4454b2",
+    "url": "/static/js/runtime-main.e0818719.js"
+  },
+  {
+    "revision": "ad42a43573e16d8add2a77de28a9c6ea",
+    "url": "/static/media/email.ad42a435.svg"
+  },
+  {
+    "revision": "cd6d72f06496dee134b2ca41662c3508",
+    "url": "/static/media/github.cd6d72f0.svg"
+  },
+  {
+    "revision": "95d6da8d1108609f30b573c5d63dd7d5",
+    "url": "/static/media/linkedin.95d6da8d.svg"
+  },
+  {
+    "revision": "23572272dca05d2598efce54cf010282",
+    "url": "/static/media/telegram.23572272.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/godested/resume/precache-manifest.9386c9f30f27772a0a2bf1cc619dbe53.js"
+  "/precache-manifest.d746f6b53b4d0ea6681a89b7363bef5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/godested/resume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
